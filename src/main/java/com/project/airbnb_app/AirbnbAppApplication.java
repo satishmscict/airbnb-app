f10ca03e5d374d5e4e629ad00bd1960e0ee866d9
Sathish <150118229+satishmscict@users.noreply.github.com>
2025-05-13 09:50:08 +0530

    Initial commit

---
+++ src/main/java/com/project/airbnb_app/AirbnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.airbnb_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbAppApplication.class, args);
+	}
+
+}
